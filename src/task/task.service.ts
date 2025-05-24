bf9f5de094c852f6e4d42c97dd7183e7d8516d73
EliasM07 <elias.mena@bolsiyo.app>
2025-05-24 13:45:23 -0600

    Config

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -20,7 +20,7 @@ export class TaskService {
     return hour >= 0 && hour < 12 ? 'AM' : 'PM';
   }
 
-  @Cron(CronExpression.EVERY_MINUTE, {
+  @Cron(CronExpression.EVERY_WEEKEND, {
     name: 'citas antes es mero dia',
     timeZone: 'America/Mexico_City',
   }) // sábado a las 00:00
